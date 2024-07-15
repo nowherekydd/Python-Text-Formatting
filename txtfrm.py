e591acd6843a3d98e08c432aba0c844c60d61011
nowherekydd <james.burnham.2001@gmail.com>
2024-07-14 20:36:39 -0400

    Update txtfrm.py
    Started adding "advanced colors" to color definition list. Still need to process them into color functions + add them to the demo list.

--- txtfrm.py
+++ txtfrm.py
@@ -18,7 +18,7 @@
 # WINDOWS - Windows 11, Thonny IDE, Windows PowerShell
 # ARCH - Arch Linux, Thonny IDE, GNOME Terminal
 
-# Last tested: 7/14/2024 @ 1917 on Windows
+# Last tested: 7/14/2024 @ 1955 on ARCH
 
 ### Basic formatting ###
 
@@ -46,6 +46,7 @@ def rgb(r, g, b):
     return f"\033[38;2;{r};{g};{b}m"
 
 # RGB codes.
+## Basic rainbow
 red = rgb(255, 0, 0)
 orange = rgb(255, 128, 0)
 yellow = rgb(255, 255, 0)
@@ -55,42 +56,81 @@ blue = rgb(0, 0, 255)
 purple = rgb(125, 0, 255)
 magenta = rgb(255, 0, 255)
 
+## Advanced colors
+### Black/White/Grey
+silver = rgb(192, 192, 192)
+grey = rgb(128, 128, 128)
+### Red
+maroon = rgb(128, 0, 0) # NTS: adv colors with # after them need to be added to final format + printlist
+terracotta = rgb(205, 92, 92) #
+### Orange
+coral = rgb(240, 128, 128) #
+salmon = rgb(233, 150, 122) #
+### Yellow
+gold = rgb(255, 215, 0) #
+goldenrod = rgb(218, 165, 32) #
+### Green
+olive = rgb(128, 128, 0) #
+chartreuse = rgb(127, 255, 0) #
+### Blue
+slategrey = rgb(47, 79, 79) #
+navy = rgb(0, 0, 128) #
+midnightblue = rgb(25, 25, 112) #
+royalblue = rgb(65, 105, 225) #
+### Purple
+indigo = rgb(75, 0, 130) #
+orchid = rgb(186, 85, 211) #
+### Brown
+brown = rgb(165, 42, 42) #
+khaki = rgb(240, 230, 140) #
+
 # Final formatting.
+## Basic
 def cred(text):
     return red + text + reset
-
 def corange(text):
     return orange + text + reset
-
 def cyellow(text):
     return yellow + text + reset
-
 def cgreen(text):
     return green + text + reset
-
 def cltblue(text):
     return ltblue + text + reset
-
 def cblue(text):
     return blue + text + reset
-
 def cpurple(text):
     return purple + text + reset
-
 def cmagenta(text):
     return magenta + text + reset
 
-### Debug ###
-# I reccommend you to delete anything beyond this comment if you're using this .py script in another project by importing it.
-print("Python Formatting Helper by nowherekydd")
+## Advanced
+### Black/White
+def csilver(text):
+    return silver + text + reset
+def cgrey(text):
+    return grey + text + reset
+
+### Red
+### Orange
+### Yellow
+### Green
+### Blue
+### Purple
+### Brown
+
+### Demo ###
+# I recommend you to delete anything beyond this comment if you're using this .py script in another project by importing it.
+print(bold("Python Formatting Helper by nowherekydd"))
+print(italic("This helper includes a linebreak function (linebreak())"))
 linebreak()
-print("Formatting")
+print(boldtal("Formatting"))
 print(bold("This text is bold."))
 print(italic("This text is italicized."))
 print(underl("This text is underlined."))
-print(boldtal("This text is bold and italicized under one function."))
+print(boldtal("This text is bold and italicized."))
 linebreak()
-print("Colors")
+print(boldtal("Colors"))
+print(italic("Basic Colors"))
 print(cred("This text is red."))
 print(corange("This text is orange."))
 print(cyellow("This text is yellow."))
@@ -99,6 +139,10 @@ print(cltblue("This text is light blue."))
 print(cblue("This text is blue."))
 print(cpurple("This text is purple."))
 print(cmagenta("This text is magenta."))
+print(italic("Advanced Colors"))
+print(csilver("This text is silver."))
+print(cgrey("This text is grey."))
+linebreak()
 
 # You can combine different formats by doing something similar to this.
 # For common combos, I'll be making their own functions.
